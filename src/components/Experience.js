3d7c7ecbeedfbc53ed5186f5958f946291c3516f
cjpanda <verifiedceejay@gmail.com>
2023-12-11 22:03:31 +0100

    Resume Update

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,103 +1,113 @@
-import React, { useRef } from 'react';
-import { useScroll, motion } from 'framer-motion';
-import LiIcon from './LiIcon';
-
+import React, { useRef } from "react";
+import { useScroll, motion } from "framer-motion";
+import LiIcon from "./LiIcon";
 
 const Details = ({ position, company, time, address, work }) => {
   const ref = useRef(null);
 
   return (
-  <li 
-  ref={ref} 
-  className='my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between md:w-[80%]'
-  >
-    <LiIcon reference={ref} />
-    <motion.div
-    initial={{y:50}}
-    whileInView={{y:0}}
-    transition={{duration: 0.5, type:"spring"}}
+    <li
+      ref={ref}
+      className="my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between md:w-[80%]"
     >
-      <h3 className='capitalize font-bold text-2xl sm:text-xl xs:text-lg'>{position}&nbsp;
-          <a className='text-primary dark:text-primaryDark capitalize cursor-pointer'>
+      <LiIcon reference={ref} />
+      <motion.div
+        initial={{ y: 50 }}
+        whileInView={{ y: 0 }}
+        transition={{ duration: 0.5, type: "spring" }}
+      >
+        <h3 className="capitalize font-bold text-2xl sm:text-xl xs:text-lg">
+          {position}&nbsp;
+          <a className="text-primary dark:text-primaryDark capitalize cursor-pointer">
             @{company}
-          </a> 
-      </h3>
-      <span className='capitalize font-medium text-dark/75 dark:text-light/75 xs:text-sm'>
-        {time} | {address}
-      </span>
-      <p className='font-medium w-full md:text-sm'>
-        {work}
-      </p>
-    </motion.div>
-  </li>
-  )
-}
+          </a>
+        </h3>
+        <span className="capitalize font-medium text-dark/75 dark:text-light/75 xs:text-sm">
+          {time} | {address}
+        </span>
+        <p className="font-medium w-full md:text-sm">{work}</p>
+      </motion.div>
+    </li>
+  );
+};
 
 const Experience = () => {
   const ref = useRef(null);
-  const {scrollYProgress} = useScroll (
-    {
-      target: ref,
-      offset: ["start end", "center start"]
-    }
-  )
-  
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start end", "center start"],
+  });
+
   return (
-    <div className='my-64 '>
-      <h2 className='font-bold text-8xl mb-32 w-full text-center md:text-6xl xs:text-4xl md:mb-16'>
+    <div className="my-64 ">
+      <h2 className="font-bold text-8xl mb-32 w-full text-center md:text-6xl xs:text-4xl md:mb-16">
         Experience
       </h2>
 
-      <div ref={ref} className='w-[75%] mx-auto relative lg:w-[90%] md:w-full'>
+      <div ref={ref} className="w-[75%] mx-auto relative lg:w-[90%] md:w-full">
+        <motion.div
+          style={{ scaleY: scrollYProgress }}
+          className="absolute left-9 top-0 w-[4px] h-full bg-dark dark:bg-light origin-top md:w-[2px] md:left-[30px] xs:left-[20px] "
+        />
 
-        <motion.div 
-          style={{scaleY: scrollYProgress}}
-          className='absolute left-9 top-0 w-[4px] h-full bg-dark dark:bg-light origin-top md:w-[2px] md:left-[30px] xs:left-[20px] ' />
+        <ul className="w-full flex flex-col items-start justify-between ml-4 xs:ml-2">
+          <Details
+            position="Frontend Web Developer"
+            company="Freelancer"
+            time="January 2023 - Present"
+            address="Remote"
+            work="• I developed fully responsive websites using React.js, translating Figma designs
+into high-quality, user-centric interfaces.
+• Provided expertise in fixing HTML, CSS, JavaScript, and React bugs, ensuring
+seamless functionality and optimal user experiences.
+• Spearheaded the creation of custom websites using a tech stack comprising
+React.js, Tailwind CSS, and Next.js, resulting in visually appealing and performant
+web solutions."
+          />
 
-            <ul className='w-full flex flex-col items-start justify-between ml-4 xs:ml-2'>
-              <Details
-                position= "FrontEnd Developer"
-                company= "CoverApp Digital"
-                time= "2020-2022"
-                address= "Kyrenia, North Cyprus"
-                work="Developing innovative web and mobile applications.
-                Maintenance and further development of existing application.
-                Design and development of database models.
-                Converting Figm Designs to a Fully Responsive Website.
-                Optimization of backend processes and database queries."
-              />
-              
-              <Details
-                position= "Web Designer"
-                company= "First World Co Ltd."
-                time= "2017-2019"
-                address= "Lagos State, Nigeria"
-                work="Planning, creation, and maintenance of websites, online portals, and
-                  internet presences.
-                  Development of the layout (colors, images, graphics, animations ).
-                  HTML/CSS programming.
-                  Upload and manage files and website content.
-                  Website publishing, hosting, and domain management.
-                  Newsletter - creation.
-                  Corporate Design.
-                  Help with marketing."
-              />
-              <Details
-                position= "IT Support"
-                company= "Dangote Flour Mill."
-                time= "2015-2017"
-                address= "CrossRiver State, Nigeria"
-                work="Responding to customer inquiries and providing technical support via phone, email, or chat.
-                Troubleshooting and diagnosing technical issues, and providing solutions in a timely manner.
-                Escalating more complex issues to 2nd or 3rd-level support.
-                Maintaining records of customer interactions and keeping track of technical issues and resolutions.
-                Collaborating with other team members to improve support processes and knowledge base articles."
-              />
-            </ul>
+          <Details
+            position="Web Developer"
+            company="CoverApp Digital"
+            time="2020-2022"
+            address="Kyrenia, North Cyprus"
+            work="• Designed user-friendly and visually appealing user interfaces to enhance the
+overall user experience.
+• Worked well independently and within a team of 5 developers.
+• Worked closely with the UI/UX team and translated Figma designs into fully
+responsive user interfaces.
+• Utilized React to create a single-page application that improved page load times
+by 50%.
+• Optimized web page loading times by 30%, improving user experience and
+engagement.
+• Developed a responsive website for an e-commerce company that increased user
+engagement and page load times by 30%
+•Collaborated with cross-functional teams to identify customer requirements and
+develop technical solutions.
+• Integrated APIs and third-party services to expand the functionality of the apps
+and improve user engagement.
+• Developed full-stack web applications from scratch, including back-end services,
+front-end interface, and database"
+          />
 
+          <Details
+            position="Junior Frontend Developer"
+            company="First World Connect Ltd."
+            time="2017-2019"
+            address="Lagos State, Nigeria"
+            work="• Successfully identified, diagnosed, and fixed website problems, including broken
+links, typographical errors, and formatting issues.
+• Helped to achieve a consistent look and visual theme across the website by
+promoting uniform fonts, formatting, images, and layout.
+• Worked to assess competing websites regarding content, look, and feel.
+• Working closely with our product UX team to find practical and efficient solutions
+to complex problems.
+• Developed a secure, interactive web application with a modern front-end
+framework and an intuitive user interface"
+          />
+        </ul>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Experience
+export default Experience;
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,195 +1,226 @@
-import React from 'react';
-import Head from 'next/head';
-import Layout from '@/components/Layout';
-import AnimatedText from '@/components/AnimatedText';
-import Link from 'next/link';
-import Image from 'next/image';
-import { GithubIcon } from '@/components/Icons';
-import project1 from '../../public/images/projects/FullResponsiveLandingPage.png';
-import project2 from '../../public/images/projects/ecommerceshop.png';
-import project3 from '../../public/images/projects/responsiveproject.png';
-import project4 from '../../public/images/projects/promptpal.png';
-import project5 from '../../public/images/projects/dashboard.png';
-import project6 from '../../public/images/projects/fullstackblog.png'
-import project7 from '../../public/images/projects/portfolio-page.png'
-import { motion } from 'framer-motion';
-import TransitionEffect from '@/components/TransitionEffect';
-
+import React from "react";
+import Head from "next/head";
+import Layout from "@/components/Layout";
+import AnimatedText from "@/components/AnimatedText";
+import Link from "next/link";
+import Image from "next/image";
+import { GithubIcon } from "@/components/Icons";
+import project1 from "../../public/images/projects/FullResponsiveLandingPage.png";
+import project2 from "../../public/images/projects/CarlandRental.png";
+import project3 from "../../public/images/projects/responsiveproject.png";
+import project4 from "../../public/images/projects/promptpal.png";
+import project5 from "../../public/images/projects/dashboard.png";
+import project6 from "../../public/images/projects/fullstackblog.png";
+import project7 from "../../public/images/projects/portfolio-page.png";
+import { motion } from "framer-motion";
+import TransitionEffect from "@/components/TransitionEffect";
 
 const FramerImage = motion(Image);
 
-
-const FeaturedProject = ({ type, title, summary, img, link, github}) => {
+const FeaturedProject = ({ type, title, summary, img, link, github }) => {
   return (
-    <article className='w-full flex items-center justify-between rounded-3xl border-4 border-solid 
-      border-dark bg-light shadow-2xl dark:bg-dark dark:border-light p-12 relative lg:flex-col lg:p-8 xs:p-4'>
-      <div className='absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-3xl bg-dark xs:-right-2 sm:h-[102%] xs:w-full' />
-      <Link href={link} target="_blank"
-      className='w-1/2 cursor-pointer overflow-hidden rounded-3xl lg:w-full'
+    <article
+      className="w-full flex items-center justify-between rounded-3xl border-4 border-solid 
+      border-dark bg-light shadow-2xl dark:bg-dark dark:border-light p-12 relative lg:flex-col lg:p-8 xs:p-4"
+    >
+      <div className="absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-3xl bg-dark xs:-right-2 sm:h-[102%] xs:w-full" />
+      <Link
+        href={link}
+        target="_blank"
+        className="w-1/2 cursor-pointer overflow-hidden rounded-3xl lg:w-full"
       >
-        <FramerImage 
-        src={img} 
-        alt={title} 
-        className='w-full h-auto' 
-        whileHover={{scale:1.05}}
-        transition={{duration:0.2}}
-        priority 
-        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 50vw"
+        <FramerImage
+          src={img}
+          alt={title}
+          className="w-full h-auto"
+          whileHover={{ scale: 1.05 }}
+          transition={{ duration: 0.2 }}
+          priority
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 50vw"
         />
       </Link>
 
-      <div className='w-1/2 flex flex-col items-start justify-between  pl-6 lg:w-full lg:pl-0 lg:pt-6'>
-        <span className='text-primary font-medium text-xl dark:text-primaryDark xs:text-base '>{type}</span>
-        <Link href={link} target="_blank" className='hover:underline underline-offset-2'>
-          <h2 className='my-2 w-full text-left text-4xl font-bold sm:text-sm'>{title}</h2>
+      <div className="w-1/2 flex flex-col items-start justify-between  pl-6 lg:w-full lg:pl-0 lg:pt-6">
+        <span className="text-primary font-medium text-xl dark:text-primaryDark xs:text-base ">
+          {type}
+        </span>
+        <Link
+          href={link}
+          target="_blank"
+          className="hover:underline underline-offset-2"
+        >
+          <h2 className="my-2 w-full text-left text-4xl font-bold sm:text-sm">
+            {title}
+          </h2>
         </Link>
-        <p className='my-2 font-medium text-dark dark:text-light sm:text-sm '>{summary}</p>
-        <div className='mt-2 flex items-center '>
-          <Link href={github} target="_blank" className='w-10'><GithubIcon/></Link>
-          <Link href={link} target="_blank" className='ml-4 rounded-lg bg-dark text-light p-2 px-6 text-lg sm:px-4 sm:text-base  font-semibold dark:bg-light dark:text-dark hover:dark:bg-primaryDark hover:bg-purple-900'>
-            Visit Project 
+        <p className="my-2 font-medium text-dark dark:text-light sm:text-sm ">
+          {summary}
+        </p>
+        <div className="mt-2 flex items-center ">
+          <Link href={github} target="_blank" className="w-10">
+            <GithubIcon />
+          </Link>
+          <Link
+            href={link}
+            target="_blank"
+            className="ml-4 rounded-lg bg-dark text-light p-2 px-6 text-lg sm:px-4 sm:text-base  font-semibold dark:bg-light dark:text-dark hover:dark:bg-primaryDark hover:bg-purple-900"
+          >
+            Visit Project
           </Link>
         </div>
       </div>
     </article>
-  )
-}
+  );
+};
 
-const Project = ({title, type, img, link, github }) => {
+const Project = ({ title, type, img, link, github }) => {
   return (
-    <article className='w-full flex flex-col items-center justify-center rounded-3xl border-4 border-solid border-dark bg-light dark:bg-dark dark:border-light dark:border-4 p-6 relative xs:p-4:'>
-            <div className='absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-3xl bg-dark md:-right-2 md:[101%] xs:h-[102%]   ' />
-      <Link href={link} target="_blank"
-      className='w-full cursor-pointer overflow-hidden rounded-3xl'
+    <article className="w-full flex flex-col items-center justify-center rounded-3xl border-4 border-solid border-dark bg-light dark:bg-dark dark:border-light dark:border-4 p-6 relative xs:p-4:">
+      <div className="absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-3xl bg-dark md:-right-2 md:[101%] xs:h-[102%]   " />
+      <Link
+        href={link}
+        target="_blank"
+        className="w-full cursor-pointer overflow-hidden rounded-3xl"
       >
-        <FramerImage 
-        src={img} 
-        alt={title} 
-        className='w-full h-auto' 
-        whileHover={{scale:1.05}}
-        transition={{duration:0.2}}
-        priority 
-        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 50vw"
+        <FramerImage
+          src={img}
+          alt={title}
+          className="w-full h-auto"
+          whileHover={{ scale: 1.05 }}
+          transition={{ duration: 0.2 }}
+          priority
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 50vw"
         />
       </Link>
 
-      <div className='w-full flex flex-col items-start justify-between mt-4'>
-        <span className='text-primary font-medium dark:text-primaryDark text-xl lg:text-lg md:text-base'>{type}</span>
-        <Link href={link} target="_blank" className='hover:underline underline-offset-2'>
-          <h2 className='my-2 w-full text-left text-3xl font-bold lg:text-2xl'>{title}</h2>
+      <div className="w-full flex flex-col items-start justify-between mt-4">
+        <span className="text-primary font-medium dark:text-primaryDark text-xl lg:text-lg md:text-base">
+          {type}
+        </span>
+        <Link
+          href={link}
+          target="_blank"
+          className="hover:underline underline-offset-2"
+        >
+          <h2 className="my-2 w-full text-left text-3xl font-bold lg:text-2xl">
+            {title}
+          </h2>
         </Link>
-        <div className='w-full mt-2 flex items-center justify-between'>
-        <Link href={link} target="_blank" className=' rounded-lg bg-dark md:text-base dark:bg-light dark:text-dark text-light p-2 px-6 text-lg font-semibold hover:dark:bg-primaryDark hover:bg-purple-900 '>
-            Visit 
+        <div className="w-full mt-2 flex items-center justify-between">
+          <Link
+            href={link}
+            target="_blank"
+            className=" rounded-lg bg-dark md:text-base dark:bg-light dark:text-dark text-light p-2 px-6 text-lg font-semibold hover:dark:bg-primaryDark hover:bg-purple-900 "
+          >
+            Visit
           </Link>
-          <Link href={github} target="_blank" className='w-8 md:w-6'><GithubIcon/>{" "}</Link>
-          
+          <Link href={github} target="_blank" className="w-8 md:w-6">
+            <GithubIcon />{" "}
+          </Link>
         </div>
       </div>
     </article>
-  )
-}
-
-
+  );
+};
 
 const projects = () => {
   return (
-  <>
-    <Head>
+    <>
+      <Head>
         <title>cjpanda | Projects</title>
-        <meta name='Portfolio Projects' content='Browse through my projects'/>
-    </Head>
-    <TransitionEffect />
-    <main className='w-full mb-16 flex flex-col items-center justify-center dark:text-light'>
-      <Layout className='pt-16'>
-        <AnimatedText text="Project Library" className='mb-16 lg:!text-7xl sm:mb-8 sm:!text-6xl xs:!text-4xl'/>
+        <meta name="Portfolio Projects" content="Browse through my projects" />
+      </Head>
+      <TransitionEffect />
+      <main className="w-full mb-16 flex flex-col items-center justify-center dark:text-light">
+        <Layout className="pt-16">
+          <AnimatedText
+            text="Project Library"
+            className="mb-16 lg:!text-7xl sm:mb-8 sm:!text-6xl xs:!text-4xl"
+          />
 
-        <div className='grid grid-cols-12 gap-24 gap-y-32 xl:gap-x-16 lg:gap-x-8 md:gap-y-24 sm:gap-x-0'>
-
-          <div className='col-span-12'>
-            <FeaturedProject 
-              title= "Ecommerce Dashboard + Store"
-              img={project5}
-              summary=" This dashboard features one or more Ecommerce stores.
+          <div className="grid grid-cols-12 gap-24 gap-y-32 xl:gap-x-16 lg:gap-x-8 md:gap-y-24 sm:gap-x-0">
+            {/* Project 1 */}
+            <div className="col-span-12">
+              <FeaturedProject
+                title="Ecommerce Dashboard + Store"
+                img={project5}
+                summary=" This dashboard features one or more Ecommerce stores.
               Developed Using: Reactjs, NextJs, Typescript, Shadcn UI, Database:Planetscale(MySQL), PrismaDB, 
               TailwindCSS, Stripe API, Clerk for User Account and Auth, Cloudinary for Image Uploads."
-              link=" https://ecommerce-dashboard-theta-nine.vercel.app"
-              github="https://github.com/cjpanda/ecommerce-dashboard"
-              type="Featured Project(Fullstack)"
-            />
-          </div>
-
-          <div className='col-span-6 sm:col-span-12'>
-            <Project 
-              title= "HooBank Landing Page"
-              img={project1}
-              link="https://cjpanda.github.io/landingpage_bank/"
-              github="https://github.com/cjpanda/landingpage_bank"
-              type="FrontEnd Project"
-            />
-          </div>
-
-          <div className='col-span-6 sm:col-span-12'>
-            <Project 
-              title= "GPT3 Landing Page"
-              img={project3}
-              link="https://cjpanda.github.io/gpt3-ben/"
-              github="https://github.com/cjpanda/gpt3-ben"
-              type="Frontend Project"
-            />
-          </div>
-
-          <div className='col-span-12'>
-            <FeaturedProject 
-              title= "Ecommerce Store"
-              img={project2}
-              summary=" An Ecommerce Store Powered by Ecommerce Dashboard project.
-              Developed Using: Reactjs, NextJs, Typescript, 
-              TailwindCSS."
-              link=" https://ecommerce-store-chi-six.vercel.app/"
-              github="https://github.com/cjpanda/ecommerce-store"
-              type="Featured Project(Fullstack)"
-            />
-          </div>
-
-    
-          <div className='col-span-6 sm:col-span-12'>
-            <Project 
-              title= "Blog"
-              img={project6}
-              link="https://github.com/cjpanda/fullstackblog_project"
-              github="https://github.com/cjpanda/fullstackblog_project"
-              type="Fullstack Project"
-            />
-          </div>
-
-          <div className='col-span-6 sm:col-span-12'>
-            <Project 
-              title= "My Portfolio"
-              img={project7}
-              link="https://cjpanda.github.io/MyPortfolio/"
-              github="https://github.com/cjpanda/MyPortfolio"
-              type="Frontend Project"
-            />
-          </div>
-
-          <div className='col-span-12'>
-            <FeaturedProject 
-              title= "Project PromptPal"
-              img={project4}
-              summary=" PromptPal is an open-source tool to create and share creative AI prompts. Features Include; User Login with GoogleAuth, User Profile, Create Save and Delete Prompts, NextAuth for Authentication.
+                link=" https://ecommerce-dashboard-theta-nine.vercel.app"
+                github="https://github.com/cjpanda/ecommerce-dashboard"
+                type="Featured Project(Fullstack)"
+              />
+            </div>
+            {/* Project 2 small */}
+            <div className="col-span-6 sm:col-span-12">
+              <Project
+                title="HooBank Landing Page"
+                img={project1}
+                link="https://cjpanda.github.io/landingpage_bank/"
+                github="https://github.com/cjpanda/landingpage_bank"
+                type="FrontEnd Project"
+              />
+            </div>
+            {/* Project 3 small */}
+            <div className="col-span-6 sm:col-span-12">
+              <Project
+                title="GPT3 Landing Page"
+                img={project3}
+                link="https://cjpanda.github.io/gpt3-ben/"
+                github="https://github.com/cjpanda/gpt3-ben"
+                type="Frontend Project"
+              />
+            </div>
+            {/* Project 4 */}
+            <div className="col-span-12">
+              <FeaturedProject
+                title="CarLand Rental "
+                img={project2}
+                summary="A Responsive Car Rental website.
+                Stack: React, NextJs, TailwindCSS, Framer Motion, React Icons, Headless UI"
+                link="https://carland-rental.vercel.app/"
+                github="https://github.com/cjpanda/CarlandRental"
+                type="Featured Project"
+              />
+            </div>
+            {/* Project 5 small */}
+            <div className="col-span-6 sm:col-span-12">
+              <Project
+                title="Blog"
+                img={project6}
+                link="https://github.com/cjpanda/fullstackblog_project"
+                github="https://github.com/cjpanda/fullstackblog_project"
+                type="Fullstack Project"
+              />
+            </div>
+            {/* Project 6 small */}
+            <div className="col-span-6 sm:col-span-12">
+              <Project
+                title="My Portfolio"
+                img={project7}
+                link="https://cjpanda.github.io/MyPortfolio/"
+                github="https://github.com/cjpanda/MyPortfolio"
+                type="Frontend Project"
+              />
+            </div>
+            {/* Project 7 */}
+            <div className="col-span-12">
+              <FeaturedProject
+                title="Project PromptPal"
+                img={project4}
+                summary=" PromptPal is an open-source tool to create and share creative AI prompts. Features Include; User Login with GoogleAuth, User Profile, Create Save and Delete Prompts, NextAuth for Authentication.
               This Project was developed with NextJS, TailwindCSS, MongoDB, GoogleAuth, NextAuth"
-              link="https://promptpal-project-nyk79e276-cjpanda.vercel.app/"
-              github="https://github.com/cjpanda/promptpal_project"
-              type="Featured Project(FullStack)"
-            />
+                link="https://promptpal-project-nyk79e276-cjpanda.vercel.app/"
+                github="https://github.com/cjpanda/promptpal_project"
+                type="Featured Project(FullStack)"
+              />
+            </div>
           </div>
+        </Layout>
+      </main>
+    </>
+  );
+};
 
-        </div>
-      </Layout>
-    </main>
-  </>
-  )
-}
-
-export default projects
+export default projects;
